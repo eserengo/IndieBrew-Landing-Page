7d5ff266c861b4171b4bfad145e63bf48fc6bc0e
eserengo <eserengo@gmail.com>
2023-10-30 20:20:05 -0300

    Adde base files - vite, vue, sass

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "App",
+  }
+</script>
+
+<template>
+  <h1>Hello</h1>
+</template>
+
+<style lang="scss">
+@use "sass:color";
+@import "main.scss";
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "HomeView"
+}
+</script>
+
+<template>
+  <h2>Home</h2>
+</template>
+
+<style lang="scss" scoped>
+@import "../main.scss";
+</style>
